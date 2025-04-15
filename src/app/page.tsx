d5a83e7aa525c5975ebf82c74d078b6453533f69
ReAuCoin <lucasbolla.uk@gmail.com>
2025-04-15 18:28:02 +0100

    New Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Counter from '@/components/Counter'
 import Footer from '@/components/Footer'
+import TotalDividendsPanel from '@/components/TotalDividendsPanel'
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
         <main className="flex-1 flex flex-col items-center justify-center px-4 md:px-12 text-center space-y-12">
           <Hero />
           <Counter />
+          <TotalDividendsPanel />
         </main>
 
         <Footer />
